1482f9742333bb5bd723e960547ee8329a9089ac
zr9796 <zr9796@gmail.com>
2020-06-16 00:00:44 +0800

    Pytest Cal Test

--- pytestcode/conftest.py
+++ pytestcode/conftest.py
@@ -2,10 +2,14 @@ import pytest
 
 @pytest.fixture(autouse=True, scope="module")
 def login():
+    print("开始计算")
     print("***I've been logged in***")
-    return ['zr9796', '970906']
+    yield ['zr9796', '123456']
+    print("计算结束")
 
 @pytest.fixture()
 def login1():
     print("***I've been logged in***")
     return ['abc', '123456']
+
+    
--- pytestcode/test_cal.py
+++ pytestcode/test_cal.py
@@ -1,51 +1,29 @@
 import pytest
+import yaml
 from cal import Calculator
 
 
-def setup_module():
-    print("开始计算")
-    
-def teardown_module():
-    print("计算结束")
-
 class TestCal:
-
+    
     def setup(self):
         self.cal = Calculator()
 
-    def test_add(self):
-        assert 3 == self.cal.add(1, 2)
+    @pytest.mark.parametrize('a,b,c', yaml.safe_load(open('./data.yml', 'r'))['add'])
+    def test_add(self, a, b, c):
+        print(f"测试 {a} + {b} = {c}")
+        assert c == self.cal.add(a, b)
     
-    def test_add1(self):
-        assert 300 == self.cal.add(100, 200)
+    @pytest.mark.parametrize('a,b,c', yaml.safe_load(open('./data.yml', 'r'))['sub'])
+    def test_sub(self, a, b, c):
+        print(f"测试 {a} - {b} = {c}")
+        assert c == self.cal.sub(a, b)
     
-    def test_add2(self):
-        assert 6.6 == self.cal.add(2.72, 3.88)
-    
-    def test_sub(self):
-        assert 1 == self.cal.sub(3, 2)
-    
-    def test_sub1(self):
-        assert 100 == self.cal.sub(300, 200)
-    
-    def test_sub2(self):
-        assert 3.38 == self.cal.sub(6.5, 3.12)
-    
-    def test_mul(self):
-        assert 6 == self.cal.mul(2, 3)
-    
-    def test_mul1(self):
-        assert 600 == self.cal.mul(20, 30)
-    
-    def test_mul2(self):
-        assert 700 == self.cal.mul(200, 3.5)
-    
-    def test_div(self):
-        assert 1 == self.cal.div(1, 1)
-    
-    def test_div1(self):
-        assert 20 == self.cal.div(60, 3)
-    
-    def test_div2(self):
-        assert 5 == self.cal.div(6, 1.2)
-        
+    @pytest.mark.parametrize('a,b,c', yaml.safe_load(open('./data.yml', 'r'))['mul'])
+    def test_mul(self, a, b, c):
+        print(f"测试 {a} * {b} = {c}")
+        assert c == self.cal.mul(a, b)
+
+    @pytest.mark.parametrize('a,b,c', yaml.safe_load(open('./data.yml', 'r'))['div'])
+    def test_div(self, a, b, c):
+        print(f"测试 {a} / {b} = {c}")
+        assert c == self.cal.div(a, b)
